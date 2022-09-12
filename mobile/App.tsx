fd669915e830337e95b4d09f5d0d4c10f289a8ea
Vinicius Lima <viniciuslimax10@gmail.com>
2022-09-11 23:25:52 -0300

    DAY 1 NLW E-SPORTS COMPLETO

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ButtonProps, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
       <StatusBar style="auto" />
     </View>
   );
+}
+interface ButtonProps{
+  title:string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
